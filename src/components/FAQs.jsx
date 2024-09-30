e02f77176f13e889e42391bb08e7d8f62af58cb0
Luqman Hassan <luqman1isb@gmail.com>
2024-09-30 17:49:44 +0500

    Added FAQs and testimonials sections

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -7,7 +7,7 @@ const FAQs = () => {
   return (
     <section className="faqs">
         <div className="contaoner faqs__container">
-            <SectionHead icon={FaQuestion} title="FAQs"/>
+            <SectionHead icon={<FaQuestion/>} title="FAQs" className="faqs_head"/>
             <div className="faqs__wrapper">
                 {
                     faqs.map(({id, question, answer}) =>{
